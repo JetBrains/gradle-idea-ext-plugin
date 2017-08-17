6701e1255f69d7441b52345ee54a7c21ba70115a
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2017-08-17 13:07:29 +0300

    extract new dynamic entity

--- src/main/groovy/org/jetbrains/gradle/ext/IdeaExtPlugin.groovy
+++ src/main/groovy/org/jetbrains/gradle/ext/IdeaExtPlugin.groovy
@@ -5,7 +5,6 @@ import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
-import org.gradle.util.Configurable
 import org.gradle.util.ConfigureUtil
 
 class IdeaExtPlugin implements Plugin<Project> {
@@ -75,50 +74,6 @@ class ModuleSettings {
   }
 }
 
-
-class NestedExpando extends Expando implements Configurable<NestedExpando>  {
-
-
-  @Override
-  NestedExpando configure(Closure closure) {
-    def cloned = closure.clone()
-    cloned.setDelegate(this)
-    cloned.setResolveStrategy(Closure.DELEGATE_FIRST)
-    cloned.call()
-    return this
-  }
-
-  @Override
-  Object invokeMethod(String name, Object args) {
-    if (args instanceof Object[] && args.length == 1 && args[0] instanceof Closure) {
-      def nested = new NestedExpando()
-      nested.configure(args[0] as Closure)
-      setProperty(name, nested)
-      return nested
-    } else if (args instanceof Object[] && args.length == 1) {
-      setProperty(name, args[0])
-      return args[0]
-    } else {
-      setProperty(name, args)
-      return args
-    }
-  }
-
-  @Override
-  Object getProperty(String property) {
-    Object result = super.getProperty(property)
-    if (result != null) {
-      return result
-    } else {
-      throw new MissingPropertyException(property, NestedExpando)
-    }
-  }
-
-  @Override
-  protected Map createMap() {
-    return new LinkedHashMap()
-  }
-}
 
 class NamedSettings extends NestedExpando {
   def name
