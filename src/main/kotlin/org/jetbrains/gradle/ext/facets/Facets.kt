a522ecf7e290081e16ff389b904f23f6af35fed0
Nikita Skvortsov <nikita.skvortsov@jetbrians.com>
2017-10-05 15:56:35 +0300

    get rid of groovy reference in nested element (WIP)

--- src/main/kotlin/org/jetbrains/gradle/ext/facets/Facets.kt
+++ src/main/kotlin/org/jetbrains/gradle/ext/facets/Facets.kt
@@ -11,8 +11,13 @@ class SpringFacet(name: String, project: Project) : Facet(name, type = "spring")
     fun contexts(action: Closure<*>) {
         contexts.configure(action)
     }
-}
 
+    /* // this implementation causes test failures (wrong delegation when configuring instances inside container)
+        fun contexts(action: Action<NamedDomainObjectContainer<SpringContext>>) {
+        action.execute(contexts)
+    }
+     */
+}
 
 class SpringContext(val name: String) {
     var file: String? = null
