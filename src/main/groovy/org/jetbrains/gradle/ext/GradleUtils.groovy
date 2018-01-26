506ab8fec0c8f4b05ad52d2f9a66e17d6d404178
Paul Merlin <paul@gradle.com>
2018-01-26 17:13:46 +0100

    Utility method for creating Gradle polymorphic containers
    Moving all Gradle internal API usage in one place
    And simplify by leveraging org.gradle.api.Named

---
+++ src/main/groovy/org/jetbrains/gradle/ext/GradleUtils.groovy
@@ -0,0 +1,21 @@
+package org.jetbrains.gradle.ext
+
+import groovy.transform.PackageScope
+import org.gradle.api.ExtensiblePolymorphicDomainObjectContainer
+import org.gradle.api.Named
+import org.gradle.api.Project
+import org.gradle.api.internal.DefaultPolymorphicDomainObjectContainer
+import org.gradle.api.internal.project.ProjectInternal
+import org.gradle.internal.reflect.Instantiator
+
+/**
+ * Helpers for Gradle internal APIs.
+ */
+@PackageScope
+class GradleUtils {
+
+    static <T extends Named> ExtensiblePolymorphicDomainObjectContainer<T> polymorphicContainer(Project project, Class<T> type) {
+        def instantiator = (project as ProjectInternal).services.get(Instantiator.class)
+        return instantiator.newInstance(DefaultPolymorphicDomainObjectContainer, type, instantiator)
+    }
+}
